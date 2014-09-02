ee3803ddd59faa96e94c546ea3f30e5ced75462d
Franku <sz.frankyy@gmail.com>
2014-09-02 12:51:25 +0200

    Grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  watch: {
+    all: {
+      options: { livereload: true },
+      files: ['**/*.js','**/*.nhtml']
+    },
+    },
+    open: {
+    all: {
+        path: 'http://localhost:1337/',
+
+    }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-open');
+
+  grunt.registerTask('serve', [
+    'open',
+    'watch'
+    ]);
+
+};
+
